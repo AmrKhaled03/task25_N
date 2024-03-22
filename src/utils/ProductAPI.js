91a725d866cb8d2b43c528bda5f1aab9e81c76f9
AmrKhaled03 <kamr4014@gmail.com>
2024-03-22 19:04:23 +0200

    Update package.json and ProductAPI.js

--- src/utils/ProductAPI.js
+++ src/utils/ProductAPI.js
@@ -1,31 +1,31 @@
 // src/utils/ProductAPI.js
-const STORAGE_KEY = "products";
+const STORAGE_KEY = "items";
 
 const ProductAPI = {
   getAllProducts: () => {
-    const products = JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
-    return products;
+    const items = JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
+    return items;
   },
   getProductById: (id) => {
-    const products = ProductAPI.getAllProducts();
-    return products.find((product) => product.id === id);
+    const items = ProductAPI.getAllProducts();
+    return items.find((item) => item.id === id);
   },
-  addProduct: (product) => {
-    const products = ProductAPI.getAllProducts();
-    products.push(product);
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(products));
+  addProduct: (item) => {
+    const items = ProductAPI.getAllProducts();
+   items.push(item);
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(items));
   },
   updateProduct: (id, updatedProduct) => {
-    const products = ProductAPI.getAllProducts();
-    const index = products.findIndex((product) => product.id === id);
+    const items = ProductAPI.getAllProducts();
+    const index =items.findIndex((item) => item.id === id);
     if (index !== -1) {
-      products[index] = { ...updatedProduct, id };
-      localStorage.setItem(STORAGE_KEY, JSON.stringify(products));
+      items[index] = { ...updatedProduct, id };
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(items));
     }
   },
   deleteProduct: (id) => {
-    const products = ProductAPI.getAllProducts();
-    const updatedProducts = products.filter((product) => product.id !== id);
+    const items = ProductAPI.getAllProducts();
+    const updatedProducts = items.filter((item) => item.id !== id);
     localStorage.setItem(STORAGE_KEY, JSON.stringify(updatedProducts));
   },
 };
